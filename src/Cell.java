36c84e1f6f3cd05308e1b6a80ee3d4a2d4be51fd
Hrithik <161672570+GaLactic-Acid@users.noreply.github.com>
2025-08-18 14:20:07 +1000

    Revert "removed local contains method"
    This reverts commit 99740ff8751a70b2e123f66caf32f1389610f091.

--- src/Cell.java
+++ src/Cell.java
@@ -25,11 +25,11 @@ public class Cell extends Rectangle{
     g.drawRect(x, y, size, size);
   }
 
-  // public boolean contains(Point p) {
-  //   if(p != null) {
-  //     return x < p.x && x+size > p.x && y < p.y && y+size > p.y;
-  //   } else {
-  //     return false;
-  //   }
+  public boolean contains(Point p) {
+    if(p != null) {
+      return x < p.x && x+size > p.x && y < p.y && y+size > p.y;
+    } else {
+      return false;
+    }
   }
 }
